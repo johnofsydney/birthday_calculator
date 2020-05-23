aee777266fdeec5291465e2eab2248bfd074775a
John Coote <john_coote@rocketmail.com>
2020-05-23 10:36:49 +1000

    mostly finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+console.log("Hello World");
+
+const writeValues = () => {
+  console.log('inside the writeValues function');
+  const dob = $('#dob').val();
+  console.log(dob);
+
+  const now = moment();
+  const then = moment(dob);
+  const duration = moment.duration(now.diff(then));
+
+  $('#years-value').text(duration.as('years').toFixed(2));
+  $('#months-value').text(duration.as('months').toFixed(2));
+  $('#weeks-value').text(duration.as('weeks').toFixed(2));
+  $('#days-value').text(duration.as('days').toFixed(2));
+}
+
+$('#dob-button').on('click', writeValues);
+
